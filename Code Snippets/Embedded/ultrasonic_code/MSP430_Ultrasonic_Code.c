32f6a4ceb8f64ec64e167bd2e90fbfc5161a9bcd
Dylan <dylan.ortiz@knights.ucf.edu>
2018-09-28 14:51:29 -0400

    Updated Ultrasonic with timer interrupt

--- Code Snippets/Embedded/ultrasonic_code/MSP430_Ultrasonic_Code.c
+++ Code Snippets/Embedded/ultrasonic_code/MSP430_Ultrasonic_Code.c
@@ -2,11 +2,11 @@
 #include <stdint.h>
 #include <stdio.h>
 
-//Holds Accelerometer and Ultrasonic information
 float sensorData[6] = {0};
 unsigned int up_counter;
 unsigned int distance_cm;
 
+__interrupt void Timer0_A0(void);
 void ultrasonic_ping();
 void init_UART();
 
@@ -25,24 +25,29 @@ int main(void)
     //Configure GPIO for the ultrasonic
     //Trigger is an output
     P1DIR |= BIT5;
-    P1SEL0 = ~BIT0;
-    P1SEL1 = ~BIT0;
     P1OUT &= ~BIT5;     //Set to low to wait for initiation
 
     //Echo is an input
     P1DIR &= ~(BIT6);
-    P1IN &= ~BIT6;
-    P1SEL0 = BIT1;   //Sets this for the timer to work with it
-    P1SEL1 = BIT1;
+    P1SEL0 |= BIT1;   //Sets this for the Timer A0 to work on this input
+    P1SEL1 |= BIT1;
 
     //init_UART();
 
-    //Set up the Timer_A0 interrupts
-    //Didnt set up capture input
-    TA0CCTL0 = CM_3 | SCS | CCIE | CAP | CCIS_0;
-    //CCTL0 |= CM_3 + SCS + CCIS_0 + CAP + CCIE;
+    //Configure the clock speed here - set to 8MHz
+    CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
+    CSCTL1 = DCOFSEL_0 | DCORSEL;             // Set DCO to 8MHz
+    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
+    CSCTL0_H = 0;                             // Lock CS registers
 
-    TA0CTL = TASSEL_0 | MC_2 | ID_3 | TAIE;
+
+
+    //Set up the Timer_A0 capture compare register
+    TA0CCTL0 = CM_3 | SCS | CCIE | CAP | CCIS_3;
+
+    //Set up the timer A0 control
+    TA0CTL = TASSEL_0 | MC_2 | ID_0 | TAIE;
 
     __bis_SR_register(GIE); // Enter LPM3, interrupts enabled
     //__no_operation();                   // For debugger
@@ -61,27 +66,32 @@ int main(void)
 //Read from echo to get the time
 void ultrasonic_ping() {
 
-    //Clear the echo pin, DOES THIS DO ANYTHING?
-    //P1IN &= ~BIT6;
-
-    printf("The echo STARTS with %x\n ", P1IN);
-    fflush(stdout);
     //Ping the trigger
     P1OUT ^= BIT5;  // drive connected pin high
-//    printf("Pin out is %x\n", P1OUT);
-//    fflush(stdout);
 
     //delay 10us or 10 cycles at 1 MHz
     __delay_cycles(10); // bad way to do delay, should use timer w/ interrupts for low-power
     P1OUT ^= BIT5;  // drive connected pin low
-//    printf("Pin out is %x\n", P1OUT);
-//    fflush(stdout);
+
+    //while loop to wait for Echo to go high
+    while(P1IN & BIT6 == 0x00){
+    }
+
+    //get the current time
+    unsigned int time1 = TA0R;
+
+    //while loop to wait for the echo to go low
+    while(P1IN & BIT6 == BIT6){
+    }
+
+    //get the current time
+    unsigned int time2 = TA0R;
+    printf("The timer says that it is %i\n", TA0R);
+
+    //the difference is what we need
+    unsigned int total_time = time2 - time1;
 
     __delay_cycles(60000);
-    printf("The echo ends with %x\n ", P1IN);
-    fflush(stdout);
-    //ping the echo
-    //P1DIR ^= (BIT6);  // set connected pin to input - 0 is input
 
     // wait 50ms with timer interrupts here
     //Need to set up Timer Control, Timer Compare Control, and the hardcoded time for the delay
@@ -99,39 +109,54 @@ void ultrasonic_ping() {
     //distance = ( (duration * 340) / 100 ) / 2
 }
 
-//Output 8 bit char to UART
-void OUTA_UART(unsigned char data){
 
-    // wait for the transmit buffer to be empty before sending data
-    do{}
-    while((UCA1IFG & 0x02) == 0);
 
-    // send the data to the transmit buffer
-    UCA1TXBUF = data;
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer0_A0(void)
+{
+    printf("We out here\n");
+    fflush(stdout);
+
+    if (TA0CCTL0 & CCI)            // Raising edge
+    {
+        up_counter = TA0CCR0;      // Copy counter to variable
+    }
+
+    else                        // Falling edge
+    {
+        // Formula: Distance in cm = (Time in uSec)/58
+        distance_cm = (TA0CCR0 - up_counter)/58;
+    }
+
+    printf("The up counter is: %x\n", up_counter);
+        fflush(stdout);
+    TA0CTL &= ~TAIFG;           // Clear interrupt flag - handled
 }
 
-void init_UART(){
-    // Startup clock system with max DCO setting ~8MHz
-    CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
-    CSCTL1 = DCOFSEL_3 | DCORSEL;             // Set DCO to 8MHz
-    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
-    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
-    CSCTL0_H = 0;                             // Lock CS registers
 
-    // Configure USCI_A0 for UART mode
-    UCA1CTLW0 = UCSWRST;                      // Put eUSCI in reset
-    UCA1CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
-    // Baud Rate calculation
-    // 8000000/(16*9600) = 52.083
-    // Fractional portion = 0.083
-    // User's Guide Table 21-4: UCBRSx = 0x04
-    // UCBRFx = int ( (52.083-52)*16) = 1
-    UCA1BR0 = 52;                             // 8000000/16/9600
-    UCA1BR1 = 0x00;
-    UCA1MCTLW |= UCOS16 | UCBRF_1;
-    UCA1CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
-    UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+//USCI_A1 Interrupt Vector - Operates on a transmission recieve from UART
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    switch(__even_in_range(UCA1IV, USCI_UART_UCTXCPTIFG))
+    {
+        case USCI_NONE: break;
+        case USCI_UART_UCRXIFG:
+          //Code goes here
+          __no_operation();
+          break;
+        case USCI_UART_UCTXIFG: break;
+        case USCI_UART_UCSTTIFG: break;
+        case USCI_UART_UCTXCPTIFG: break;
+    }
 }
+
 
 //Converts float to 4 different 8 bit data points to be sent
 //over UART
@@ -168,47 +193,38 @@ void floatToBuffer(float data)
     OUTA_UART('\n');
 }
 
-//USCI_A1 Interrupt Vector - Operates on a transmission recieve from UART
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCI_A1_VECTOR
-__interrupt void USCI_A1_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
-#else
-#error Compiler not supported!
-#endif
-{
-    switch(__even_in_range(UCA1IV, USCI_UART_UCTXCPTIFG))
-    {
-        case USCI_NONE: break;
-        case USCI_UART_UCRXIFG:
-          //Code goes here
-          __no_operation();
-          break;
-        case USCI_UART_UCTXIFG: break;
-        case USCI_UART_UCSTTIFG: break;
-        case USCI_UART_UCTXCPTIFG: break;
-    }
+
+void init_UART(){
+    // Startup clock system with max DCO setting ~8MHz
+    CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
+    CSCTL1 = DCOFSEL_3 | DCORSEL;             // Set DCO to 8MHz
+    CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
+    CSCTL0_H = 0;                             // Lock CS registers
+
+    // Configure USCI_A0 for UART mode
+    UCA1CTLW0 = UCSWRST;                      // Put eUSCI in reset
+    UCA1CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
+    // Baud Rate calculation
+    // 8000000/(16*9600) = 52.083
+    // Fractional portion = 0.083
+    // User's Guide Table 21-4: UCBRSx = 0x04
+    // UCBRFx = int ( (52.083-52)*16) = 1
+    UCA1BR0 = 52;                             // 8000000/16/9600
+    UCA1BR1 = 0x00;
+    UCA1MCTLW |= UCOS16 | UCBRF_1;
+    UCA1CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
+    UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
 }
 
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer0_A0(void)
-{
-    printf("We out here\n");
-    fflush(stdout);
 
-    if (TA0CCTL0 & CCI)            // Raising edge
-    {
-        up_counter = TA0CCR0;      // Copy counter to variable
-    }
+//Output 8 bit char to UART
+void OUTA_UART(unsigned char data){
 
-    else                        // Falling edge
-    {
-        // Formula: Distance in cm = (Time in uSec)/58
-        distance_cm = (TA0CCR0 - up_counter)/58;
-    }
+    // wait for the transmit buffer to be empty before sending data
+    do{}
+    while((UCA1IFG & 0x02) == 0);
 
-    printf("The up counter is: %x\n", up_counter);
-        fflush(stdout);
-    TA0CTL &= ~TAIFG;           // Clear interrupt flag - handled
+    // send the data to the transmit buffer
+    UCA1TXBUF = data;
 }
