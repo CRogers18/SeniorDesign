956e803304e9619fda212acfa76a1ac2f3b40c89
Dylan_Ortiz <bigdmo96@gmail.com>
2018-10-09 19:01:19 -0400

    Added code

--- Code Snippets/Embedded/MSP430_Accelerometer_Code.c
+++ Code Snippets/Embedded/MSP430_Accelerometer_Code.c
@@ -22,6 +22,9 @@ void init_I2C(){
 	P1SEL0 &= ~(BIT6 | BIT7);
 	P1SEL1 |= (BIT6 | BIT7);
 
+	//Enable UCSWRST to configure everything
+	UCB0CTLW0 |= UCSWRST;
+
 	//Setup CTLW0
 	//Set UCA10 to 0 - 7 bit own addressing
 	//Set UCSLA10 to 0 - 7 bit slave addressing
@@ -32,11 +35,22 @@ void init_I2C(){
 	//Set UCSSEL0 to 11 - SMCLK
 	//Set UCTXACK to 1 - Acknowledge the slave address
 	//Set UCTR to 0 - Reciever (Since Im getting info from the Accelerometer)
-	//Set UCTXNACK - 
-	//Set UCTXSTP
-	//Set UCTXSTT
-	//Set UCSWRST to 1 - Set the reset to start
-	UCB0CTLW0 |= 
+	//Set UCTXNACK to 0 - Regualr aknowledge 
+	//Set UCTXSTP to 0 - No STOP
+	//Set UCTXSTT to 0 - No START
+	//Dont mess with UCSWRST to 1
+	UCB0CTLW0 |= UCMST | UCMODE0_3 | UCSYNC | UCSSEL0_3 | UCTXACK
 
 	//Setup CTLW1
+	//Might not need to do this
+
+	UCBR0 = 12;	//SMCLK/12 = ~100KHz???
+
+	//Slave Address - Do I need to declare this
+	//Figure out the slave address
+	UCB0I2CSA = 
+
+	//Disable UCSWRST
+	UCB0CTLW0 &= ~UCSWRST;
+
 }
